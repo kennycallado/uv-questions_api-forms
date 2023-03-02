829368d7fbb576edb6dfba179617b1e6a7c64a1e
kennycallado <kennycallado@hotmail.com>
2023-03-02 13:09:06 +0100

    UPDATE Actualiza módulo form para introducir relación con questions

--- src/app/modules/form/controller.rs
+++ src/app/modules/form/controller.rs
@@ -1,6 +1,7 @@
 use rocket::http::Status;
 use rocket::serde::json::Json;
 
+use crate::app::modules::question::model::Question;
 use crate::app::providers::guards::admin::AdminClaims;
 use crate::app::providers::guards::coord::CoordClaims;
 use crate::app::providers::guards::thera::TheraClaims;
@@ -9,6 +10,9 @@ use crate::config::database::Db;
 
 use crate::app::modules::form::model::{Form, NewForm};
 use crate::app::modules::form::services::repostitory as form_repository;
+use crate::app::modules::form_questions::services::respository as form_question_repository;
+
+use super::model::{FormWithQuestions, NewFormWithQuestions};
 
 pub fn routes() -> Vec<rocket::Route> {
     routes![
@@ -52,17 +56,35 @@ pub async fn get_index_none() -> Status {
 }
 
 #[get("/<id>", rank = 4)]
-pub async fn get_show_user(db: Db, _claims: UserClaims, id: i32) -> Result<Json<Form>, Status> {
+pub async fn get_show_user(
+    db: Db,
+    _claims: UserClaims,
+    id: i32,
+) -> Result<Json<FormWithQuestions>, Status> {
     let form = form_repository::get_by_id(&db, id).await;
-
-    match form {
-        Ok(form) => Ok(Json(form)),
-        Err(e) => {
-            println!("Module: form - Controller: get_show_user");
-            println!("Error: {:?}", e);
-            Err(Status::InternalServerError)
-        }
+    if let Err(e) = form {
+        println!("Module: form - Controller: get_show_user");
+        println!("Error: {:?}", e);
+        return Err(Status::InternalServerError);
     }
+    let form = form.unwrap();
+
+    let questions = form_question_repository::get_questions_by_form_id(&db, id).await;
+    if let Err(e) = questions {
+        println!("Module: form - Controller: get_show_user");
+        println!("Error: {:?}", e);
+        return Err(Status::InternalServerError);
+    }
+    let questions = questions.unwrap();
+
+    let form_expanded = FormWithQuestions {
+        id: form.id,
+        name: form.name,
+        description: form.description,
+        questions,
+    };
+
+    Ok(Json(form_expanded))
 }
 
 #[get("/<_id>", rank = 5)]
@@ -71,64 +93,119 @@ pub async fn get_show_none(_id: i32) -> Status {
     Status::Unauthorized
 }
 
-#[post("/", data = "<form>", rank = 1)]
-pub async fn post_create_admin(
-    db: Db,
-    _claims: AdminClaims,
-    form: Json<NewForm>,
-) -> Result<Json<Form>, Status> {
-    let form = form_repository::create(&db, form.into_inner()).await;
+async fn create_helper(db: &Db, form: NewFormWithQuestions) -> Result<(Form, Vec<Question>), Status> {
+    // Get the questions
+    let questions = match form.questions_id.clone() {
+        Some(questions) => questions,
+        None => vec![],
+    };
 
-    match form {
-        Ok(form) => Ok(Json(form)),
-        Err(e) => {
-            println!("Module: form - Controller: post_create_admin");
-            println!("Error: {:?}", e);
-            Err(Status::InternalServerError)
-        }
+    // Create the form
+    let new_form: NewForm = form.into();
+    let form = form_repository::create(&db, new_form).await;
+    if let Err(e) = form {
+        println!("Module: form - Controller: helper");
+        println!("Error: {:?}", e);
+        return Err(Status::InternalServerError);
     }
+    let form = form.unwrap();
+
+    // Add the questions to the form
+    let questions = form_question_repository::add_questions_to_form(&db, form.id, questions).await;
+    if let Err(e) = questions {
+        println!("Module: form - Controller: helper");
+        println!("Error: {:?}", e);
+        return Err(Status::InternalServerError);
+    }
+    let questions = questions.unwrap();
+
+    return Ok((form, questions));
 }
 
-#[post("/", data = "<form>", rank = 2)]
-pub async fn post_create_coord(
-    db: Db,
-    _claims: CoordClaims,
-    form: Json<NewForm>,
-) -> Result<Json<Form>, Status> {
-    let form = form_repository::create(&db, form.into_inner()).await;
+#[post("/", data = "<new_form>", rank = 1)]
+pub async fn post_create_admin(db: Db, _claims: AdminClaims, new_form: Json<NewFormWithQuestions>) -> Result<Json<FormWithQuestions>, Status> {
+    let form = new_form.into_inner();
 
-    match form {
-        Ok(form) => Ok(Json(form)),
-        Err(e) => {
-            println!("Module: form - Controller: post_create_coord");
-            println!("Error: {:?}", e);
-            Err(Status::InternalServerError)
-        }
-    }
+    let (form, questions) = create_helper(&db, form).await?;
+
+    // Prepare the response
+    let form_expanded = FormWithQuestions {
+        id: form.id,
+        name: form.name,
+        description: form.description,
+        questions,
+    };
+
+    Ok(Json(form_expanded))
 }
 
-#[post("/", data = "<form>", rank = 3)]
-pub async fn post_create_thera(
-    db: Db,
-    _claims: TheraClaims,
-    form: Json<NewForm>,
-) -> Result<Json<Form>, Status> {
-    let form = form_repository::create(&db, form.into_inner()).await;
+#[post("/", data = "<new_form>", rank = 2)]
+pub async fn post_create_coord(db: Db, _claims: CoordClaims, new_form: Json<NewFormWithQuestions>) -> Result<Json<FormWithQuestions>, Status> {
+    let form = new_form.into_inner();
 
-    match form {
-        Ok(form) => Ok(Json(form)),
-        Err(e) => {
-            println!("Module: form - Controller: post_create_thera");
-            println!("Error: {:?}", e);
-            Err(Status::InternalServerError)
-        }
-    }
+    let (form, questions) = create_helper(&db, form).await?;
+
+    // Prepare the response
+    let form_expanded = FormWithQuestions {
+        id: form.id,
+        name: form.name,
+        description: form.description,
+        questions,
+    };
+
+    Ok(Json(form_expanded))
+}
+
+#[post("/", data = "<new_form>", rank = 3)]
+pub async fn post_create_thera(db: Db, _claims: CoordClaims, new_form: Json<NewFormWithQuestions>) -> Result<Json<FormWithQuestions>, Status> {
+    let form = new_form.into_inner();
+
+    let (form, questions) = create_helper(&db, form).await?;
+
+    // Prepare the response
+    let form_expanded = FormWithQuestions {
+        id: form.id,
+        name: form.name,
+        description: form.description,
+        questions,
+    };
+
+    Ok(Json(form_expanded))
 }
 
 #[post("/", data = "<_form>", rank = 5)]
-pub async fn post_create_none(_form: Json<NewForm>) -> Status {
+pub async fn post_create_none(_form: Json<NewFormWithQuestions>) -> Status {
     println!("Module: form - Controller: post_create_none");
     Status::Unauthorized
+}
+
+async fn update_helper(db: &Db, id: i32, form: NewFormWithQuestions) -> Result<(Form, Vec<Question>), Status> {
+    // Get the questions
+    let questions = match form.questions_id.clone() {
+        Some(questions) => questions,
+        None => vec![],
+    };
+
+    // Create the form
+    let new_form: NewForm = form.into();
+    let form = form_repository::update(&db, id, new_form).await;
+    if let Err(e) = form {
+        println!("Module: form - Controller: update_helper");
+        println!("Error: {:?}", e);
+        return Err(Status::InternalServerError);
+    }
+    let form = form.unwrap();
+
+    // Add the questions to the form
+    let questions = form_question_repository::update_questions_in_form(&db, form.id, questions).await;
+    if let Err(e) = questions {
+        println!("Module: form - Controller: update_helper");
+        println!("Error: {:?}", e);
+        return Err(Status::InternalServerError);
+    }
+    let questions = questions.unwrap();
+
+    return Ok((form, questions));
 }
 
 #[put("/<id>", data = "<form>", rank = 1)]
@@ -136,18 +213,21 @@ pub async fn put_update_admin(
     db: Db,
     _claims: AdminClaims,
     id: i32,
-    form: Json<NewForm>,
-) -> Result<Json<Form>, Status> {
-    let form = form_repository::update(&db, id, form.into_inner()).await;
+    form: Json<NewFormWithQuestions>,
+) -> Result<Json<FormWithQuestions>, Status> {
+    let form = form.into_inner();
 
-    match form {
-        Ok(form) => Ok(Json(form)),
-        Err(e) => {
-            println!("Module: form - Controller: put_update_admin");
-            println!("Error: {:?}", e);
-            Err(Status::InternalServerError)
-        }
-    }
+    let (form, questions) = update_helper(&db, id, form).await?;
+
+    // Prepare the response
+    let form_expanded = FormWithQuestions {
+        id: form.id,
+        name: form.name,
+        description: form.description,
+        questions,
+    };
+
+    Ok(Json(form_expanded))
 }
 
 #[put("/<id>", data = "<form>", rank = 2)]
@@ -155,18 +235,21 @@ pub async fn put_update_coord(
     db: Db,
     _claims: CoordClaims,
     id: i32,
-    form: Json<NewForm>,
-) -> Result<Json<Form>, Status> {
-    let form = form_repository::update(&db, id, form.into_inner()).await;
+    form: Json<NewFormWithQuestions>,
+) -> Result<Json<FormWithQuestions>, Status> {
+    let form = form.into_inner();
 
-    match form {
-        Ok(form) => Ok(Json(form)),
-        Err(e) => {
-            println!("Module: form - Controller: put_update_coord");
-            println!("Error: {:?}", e);
-            Err(Status::InternalServerError)
-        }
-    }
+    let (form, questions) = update_helper(&db, id, form).await?;
+
+    // Prepare the response
+    let form_expanded = FormWithQuestions {
+        id: form.id,
+        name: form.name,
+        description: form.description,
+        questions,
+    };
+
+    Ok(Json(form_expanded))
 }
 
 #[put("/<id>", data = "<form>", rank = 3)]
@@ -174,18 +257,21 @@ pub async fn put_update_thera(
     db: Db,
     _claims: TheraClaims,
     id: i32,
-    form: Json<NewForm>,
-) -> Result<Json<Form>, Status> {
-    let form = form_repository::update(&db, id, form.into_inner()).await;
+    form: Json<NewFormWithQuestions>,
+) -> Result<Json<FormWithQuestions>, Status> {
+    let form = form.into_inner();
 
-    match form {
-        Ok(form) => Ok(Json(form)),
-        Err(e) => {
-            println!("Module: form - Controller: put_update_thera");
-            println!("Error: {:?}", e);
-            Err(Status::InternalServerError)
-        }
-    }
+    let (form, questions) = update_helper(&db, id, form).await?;
+
+    // Prepare the response
+    let form_expanded = FormWithQuestions {
+        id: form.id,
+        name: form.name,
+        description: form.description,
+        questions,
+    };
+
+    Ok(Json(form_expanded))
 }
 
 #[put("/<_id>", data = "<_form>", rank = 5)]
--- src/app/modules/form/model.rs
+++ src/app/modules/form/model.rs
@@ -1,7 +1,6 @@
-// use diesel::{types::FromSql, sql_types::{Text, Integer}, backend::Backend, deserialize};
 use serde::{Deserialize, Serialize};
 
-use crate::database::schema::forms;
+use crate::{app::modules::question::model::Question, database::schema::forms};
 
 #[derive(Debug, Clone, Deserialize, Serialize, Queryable, Identifiable, FromForm)]
 #[serde(crate = "rocket::serde")]
@@ -9,6 +8,16 @@ pub struct Form {
     pub id: i32,
     pub name: String,
     pub description: Option<String>,
+}
+
+impl From<FormWithQuestions> for Form {
+    fn from(form: FormWithQuestions) -> Self {
+        Form {
+            id: form.id,
+            name: form.name,
+            description: form.description,
+        }
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize, Insertable, AsChangeset)]
@@ -27,3 +36,47 @@ impl From<Form> for NewForm {
         }
     }
 }
+
+impl From<FormWithQuestions> for NewForm {
+    fn from(form: FormWithQuestions) -> Self {
+        NewForm {
+            name: form.name,
+            description: form.description,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct FormWithQuestions {
+    pub id: i32,
+    pub name: String,
+    pub description: Option<String>,
+    pub questions: Vec<Question>,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct NewFormWithQuestions {
+    pub name: String,
+    pub description: Option<String>,
+    pub questions_id: Option<Vec<i32>>,
+}
+
+impl From<NewFormWithQuestions> for NewForm {
+    fn from(form: NewFormWithQuestions) -> Self {
+        NewForm {
+            name: form.name,
+            description: form.description,
+        }
+    }
+}
+
+// impl From<NewFormExpanded> for NewForm {
+//     fn from(form: NewFormExpanded) -> Self {
+//         NewForm {
+//             name: form.name,
+//             description: form.description,
+//         }
+//     }
+// }
